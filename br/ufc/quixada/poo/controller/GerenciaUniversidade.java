52c2880de96d50845602c6e841f96c7df69099b5
FGabriel-Lima <gabrelgabrel621@gmail.com>
2022-06-28 13:55:37 -0300

    Update GerenciaUniversidade.java

--- br/ufc/quixada/poo/controller/GerenciaUniversidade.java
+++ br/ufc/quixada/poo/controller/GerenciaUniversidade.java
@@ -10,22 +10,33 @@ import br.ufc.quixada.poo.model.Universidade;
 public interface GerenciaUniversidade {
   // gerenciando as universidades
 
+  // adiciona uma universidade a lista de universidades
   public boolean adicionarUniversidade(Universidade universidade);
 
+  // remove uma universidade pelo nome
   public boolean removerUniversidade(String nome);
 
-  public boolean modificarDiretor(String nomeDiretor);
+  // dado um nome da univerdade e um nome, substitui o diretor da universidade
+  public boolean modificarDiretor(String nomeUniversidae, String nomeDiretor);
 
+  // retorna uma universidade dado o nome
+  public Universidade getUniversidade(String nome);
+
+  // retorna uma lista com as universidades que foram adicionadas
   public ArrayList<Universidade> imprimirListaUniversidade();
 
   // gerenciando os alunos
 
-  public boolean adicionarAluno(Aluno aluno);
+  // dado um nome da universidade e um aluno, adiciona um aluno na lista
+  public boolean adicionarAluno(String nomeUniversidade, Aluno aluno);
 
-  public boolean removerAluno(String nome);
+  // dado um nome da universidade e um nome de aluno, remove um aluno na lista
+  public boolean removerAluno(String nomeUniversidade, String nomeAluno);
 
-  public Aluno getAluno(String nome);
+  // dado uma universidade e um nome de aluno, retorna um aluno com mesmo nome
+  public Aluno getAluno(Universidade universidade, String nome);
 
+  // dado um nome d, altera o curso
   public Aluno alterarCurso(String nomeCurso);
 
   public ArrayList<Aluno> procurarCandidatos(Localizacao local, String curso);
